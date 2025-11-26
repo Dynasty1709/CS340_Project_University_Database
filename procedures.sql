a9fc3912f0bd75cdc1d1b090e44f3a7907587983
wheelken <wheelken@oregonstate.edu>
2025-11-26 16:53:35 -0600

    Delete procedures.sql

--- procedures.sql
+++
@@ -1,8 +0,0 @@
--- for required procedures
-DELIMITER //
-CREATE PROCEDURE insertUniversityMajor(IN uniID INT, IN majID INT)
-BEGIN
-  INSERT INTO UNIVERSITIES_HAS_MAJORS (universityID, majorID)
-  VALUES (uniID, majID);
-END //
-DELIMITER ;
